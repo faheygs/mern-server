d0e25cc93bb8cd3c7b02ced783c9bbfd03b8fceb
faheygs@gmail.com <faheygs@gmail.com>
2021-11-04 18:55:16 -0600

    asdf

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { path: '/socket.io'}, {
     cors: {
